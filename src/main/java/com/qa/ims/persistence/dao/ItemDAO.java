b26c8c5e2d2605c17af1acae7bc30e3e91fd7e2f
SalahS49 <salah_49@outlook.com>
2022-04-03 02:07:09 +0100

    updated item dao class

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -86,47 +86,51 @@ public class ItemDAO implements Dao<Item> {
 
 	@Override
 	public int delete(long id) {
-		  try (Connection connection = DBUtils.getInstance().getConnection();
-	                Statement statement = connection.createStatement();) {
-	            return statement.executeUpdate("delete from items where id = " + id);
-	        } catch (Exception e) {
-	            LOGGER.debug(e);
-	            LOGGER.error(e.getMessage());
-	        }
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			return statement.executeUpdate("delete from items where id = " + id);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
 		return 0;
 	}
-	
+
 	public int removeOrdersItems(Item item, long orderID) {
-        try (Connection connection = DBUtils.getInstance().getConnection();
-        PreparedStatement statement = connection
-                .prepareStatement("DELETE FROM orders_items WHERE (fk_orders_id = ? AND fk_items_id = ?)")) {
-            statement.setLong(1, orderID);
-            statement.setLong(2, item.getId());
-            return statement.executeUpdate();
-        } catch (Exception e) {
-            LOGGER.debug(e);
-            LOGGER.error(e.getMessage());
-        }
-        return 0;
-    }
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("DELETE FROM orders_items WHERE (fk_orders_id = ? AND fk_items_id = ?)")) {
+			statement.setLong(1, orderID);
+			statement.setLong(2, item.getId());
+			return statement.executeUpdate();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}
 
 	@Override
 	public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long id = resultSet.getLong("id");
+		String name = resultSet.getString("name");
+		double price = resultSet.getDouble("price");
 
+		return new Item(id, name, price);
+
+	}
+
+	public Item readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
 		return null;
 	}
-	
-	public Item readLatest() {
-        try (Connection connection = DBUtils.getInstance().getConnection();
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1");) {
-            resultSet.next();
-            return modelFromResultSet(resultSet);
-        } catch (Exception e) {
-            LOGGER.debug(e);
-            LOGGER.error(e.getMessage());
-        }
-        return null;
-    }
 
 }
